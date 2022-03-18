2b7c8ee10b25ab09528d14b42db4853f0c364cf0
Ganiyat Yakub <yakubganiyat96@gmail.com>
2022-03-18 14:17:16 +0100

    fix warning

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,7 +3,7 @@ import React from "react";
 const Project = ({ href, imgSrc, p }) => {
   return (
     <>
-      <a href={href} target="_blank" className="project project-tile">
+      <a href={href} className="project project-tile">
         <img className="project-image" src={imgSrc} alt="project" />
         <p className="project-title">{p}</p>
       </a>
